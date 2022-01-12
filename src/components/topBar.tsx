eecda1778d2606b1e490795148531031ae3bac1a
Arnaud Béguin <abeguin@kleis.ch>
2022-01-12 22:15:22 +0100

    add a simple menu

--- src/components/topBar.tsx
+++ src/components/topBar.tsx
@@ -3,15 +3,25 @@ import AppBar from "@mui/material/AppBar"
 import Box from "@mui/material/Box"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
+import tw from "twin.macro"
+import { Link } from "gatsby"
 
 const TopBar: React.FC = () => {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Meal planner
-          </Typography>
+            <Typography variant="h6" component="div">
+              Meal planner
+            </Typography>
+            <Box css={tw`flex flex-row flex-1 items-center justify-end`}>
+              <Link to={"/"} css={tw`px-4`}>
+                Plan
+              </Link>
+              <Link to={"/food"}>
+                Meals
+              </Link>
+            </Box>
         </Toolbar>
       </AppBar>
     </Box>
